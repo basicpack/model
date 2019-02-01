2602610aeeab30c33f4ae44753a2e2f79feb168b
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-02-01 09:48:05 -0200

    Update model.php

--- model.php
+++ model.php
@@ -2,24 +2,16 @@
 //Criado por Anderson Ismael
 //20 de Dezembro de 2018
 
-function model($names,$data=false){
-    if(is_array($names)){
-        foreach($names as $name){
-            openModel($name,$data);
-        }
-    }else{
-        openModel($names,$data);
-    }
-}
-
-function openModel($name,$data=false){
+function model($name,$data=false){
     $str=ROOT.'model/'.$name.'.php';
     if(file_exists($str)){
         if(is_array($data)){
             extract($data);
         }
         require_once($str);
+        return true;
     }else{
-        die('<b>Erro:</b><br>'.PHP_EOL.'<br>Model <b>'.$name.'</b> não encontrado'.PHP_EOL);
+        return false;
     }
 }
+
