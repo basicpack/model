a0471d18001384f34aacb8ed55a822479ee80084
Anderson Ismael <23392311+aicoutodasilva@users.noreply.github.com>
2019-02-03 10:07:00 -0200

    Update model.php

--- model.php
+++ model.php
@@ -2,12 +2,9 @@
 //Criado por Anderson Ismael
 //20 de Dezembro de 2018
 
-function model($name,$data=false){
+function model($name){
     $str=ROOT.'model/'.$name.'.php';
     if(file_exists($str)){
-        if(is_array($data)){
-            extract($data);
-        }
         return require_once($str);
     }else{
         return false;
