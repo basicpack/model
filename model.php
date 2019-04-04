955bde7ec99a2d3c7d423df35b5aabcb5c297a28
Andeson Ismael <23392311+aicouto@users.noreply.github.com>
2019-04-04 15:29:46 -0300

    Update model.php

--- model.php
+++ model.php
@@ -3,7 +3,7 @@
 //20 de Dezembro de 2018
 
 function model($name){
-    $str=ROOT.'model/'.$name.'.php';
+    $str=ROOT.'app/model/'.$name.'.php';
     if(file_exists($str)){
         return require_once($str);
     }else{
