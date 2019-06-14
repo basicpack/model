6ebd1f4da1d8dc85afcf6b78b7ca531bcf864fea
Anderson Ismael <23392311+aicouto@users.noreply.github.com>
2019-06-14 16:24:42 -0300

    M  model.php

--- model.php
+++ model.php
@@ -4,8 +4,14 @@
 
 function model($name){
     $filename=ROOT.'app/model/'.$name.'.php';
+    $name2=explode('/',$name);
+    $repoName=@$name2[0];
+    $modelName=@$name2[1];
+    $filename2=ROOT.$repoName.'/model/'.$modelName.'.php';
     if(file_exists($filename)){
         return require_once($filename);
+    }elseif(file_exists($filename2)){
+        return require_once($filename2);
     }else{
         die('model <b>'.$name.'</b> not found');
     }
